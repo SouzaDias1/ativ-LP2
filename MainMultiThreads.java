f65aac2509d6dfb35e6d905697d5d1b4b71dd024
Pedro Henrique <101672436+SouzaDias1@users.noreply.github.com>
2022-07-06 15:51:17 -0300

    Update MainMultiThreads.java

--- MainMultiThreads.java
+++ MainMultiThreads.java
@@ -1,15 +1,30 @@
-package MainMultiThreads;
 
-public class MainMultiThreads {
 
-  public static void MainMultiThreads (String() args)
-  throws InterruptedException {
-    ExecutorService threadRun = buffer sharedLocation = new unsysnchronizedBffer()
-      System.out.printf("Thread running \t\tValue\t" + Sum write\tSum read\n);
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
-    System.out.println("---------------------------------------");
-    threadRun.excute(new writeBuffer(saredLocation));
-    threadRun.excute(new readBuffer(saredLocation));
-    threadRun.shutdown();
-    threadRun.awaitTermination(1, TimeUnit.MINUTES);
+public class Main {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		ExecutorService  threadRun = Executors.newCachedThreadPool();
+		Buffer sharedLocation = new UnsysnchronizedBuffer();
+		
+		System.out.printf(
+			"Thread running\t\tValue\t "
+			+ "Sum write\tSum read\n"
+		);
+
+		System.out.println("-------------------------------------------------------------------------------------");
+		threadRun.execute(new WriteBuffer(sharedLocation));
+		threadRun.execute(new ReadBuffer(sharedLocation));
+		threadRun.shutdown();
+		threadRun.awaitTermination(1, TimeUnit.MINUTES);
+		System.out.println("-------------------------------------------------------------------------------------");
+		System.out.printf(
+			"Thread running\t\t\tIs\t\t\tClosing\n\n"
+		);
+	}
+
 }
