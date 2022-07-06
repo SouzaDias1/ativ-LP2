5e85235a3335949a0c262dfe729dd20a836ba637
Pedro Henrique <101672436+SouzaDias1@users.noreply.github.com>
2022-07-06 16:22:30 -0300

    Update buffer.java

--- buffer.java
+++ buffer.java
@@ -2,4 +2,8 @@ package multihreads;
 public interface Buffer {
 	public void putBuffer(int value) throws InterruptedException;
 	public int getBuffer() throws InterruptedException;
+	public void putBufferDoubleArray(double value) throws InterruptedException;
+	public double getBufferDoubleArray() throws InterruptedException;
+	public void putBufferStringArray(int value) throws InterruptedException;
+	public int getBufferStringArray() throws InterruptedException;
 }
