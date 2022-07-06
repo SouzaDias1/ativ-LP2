9883c8fa0542eb804213f231a59bea6f356be238
Pedro Henrique <101672436+SouzaDias1@users.noreply.github.com>
2022-07-06 15:49:23 -0300

    Update writeBuffer.java

--- writeBuffer.java
+++ writeBuffer.java
@@ -1,30 +1,27 @@
-package multihreads;
+import java.security.SecureRandom;
 
-import java.securiy.SecureRandom;
-
-public class Main {
-    public class writeBuffer implements Runnble{
-        private static final buffer sharedLocation;
-        public writeBuffer(buffer sharedLocation) {
-            this.sharedLocation = sharedLocation;
-        } 
-        public void run() {
-            int sum = 0;
-            for (int count = 1;count <=10; count ++){
-                try 
-                (
-                    Thread.sleep(generator.nextInt(3000));
-                    sharedLocation.putBuffer(count);
-                    sum+=count;
-                    System.out.printf("\t%2d\n", sum);
-                )
-                catch(InterruotedException e)
-                {
-                    Thread..currentThread().interrupt();
-                }
-            }
-        }
-        System.out.println("Escrita do buffer realizada!\n");
-
-    }
+public class WriteBuffer implements Runnable{
+	private static final SecureRandom 
+	generator = new SecureRandom();
+	private final Buffer sharedLocation;	
+	public WriteBuffer(Buffer sharedLocation) {
+		this.sharedLocation = sharedLocation;
+	}
+	public void run() {
+		int sum = 0;
+		for (int count =1;count <=10;count ++) {
+			try {
+				Thread.sleep(generator.nextInt(3000));
+				sharedLocation.putBuffer(count);
+				sum+=count;
+				System.out.printf("\t\t\t%2d\n",sum);
+				}
+			catch(InterruptedException e){
+				Thread.currentThread().interrupt();				
+			}			
+		}
+		System.out.printf("%n%s %d%n%s%n",
+				"Escrita do total de valores: ",
+				sum, "Escrita finalizada!");
+	}
 }
